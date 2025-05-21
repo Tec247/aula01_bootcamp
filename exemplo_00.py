98cdf2bfaf9fc0bd1367920891cbcdedcd10c272
Tec247 <pegazoz99@gmail.com>
2025-05-20 22:36:21 -0300

    adicionando o exemplo 0

---
+++ exemplo_00.py
@@ -0,0 +1,7 @@
+#Crie um programa que o usuário digite o seu nome e retorne o número de caracteres.
+
+nome = input("Digite seu nome: ")
+qtd_caracter = len(nome)
+
+print(nome)
+print(qtd_caracter)
